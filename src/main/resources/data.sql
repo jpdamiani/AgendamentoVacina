f843fad2bc7bd757f1cb07d48c8fcc914c342238
João Paulo <joaopfdamiani@gmail.com>
2021-11-06 18:09:24 -0300

    Alteracao data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
 INSERT INTO tb_usuario (nome, cpf, cep, logradouro, cidade, estado, dataNascimento, email) VALUES ('Joao', '11111111111','90250180', 'Rua dos Bobos', 'São Leopoldo', 'RS', '2001-01-07T16:11:26.485','teste1@gmail.com');
 INSERT INTO tb_usuario (nome, cpf, cep, logradouro, cidade, estado, dataNascimento, email) VALUES ('Stefano', '66363732026','90130000', 'Broadway Avenue', 'Novo Hamburgo', 'RS', '2001-03-07T16:11:26.485','teste2@hotmail.com');
 INSERT INTO tb_usuario (nome, cpf, cep, logradouro, cidade, estado, dataNascimento, email) VALUES ('Guilherme', '35684995098','95000123', 'Rua da Bruxa do 71', 'Porto Alegre', 'RS', '2001-04-07T16:11:26.485','teste3@gmail.com');
-=======
-INSERT INTO tb_usuario (nome, cpf, cep, logradouro, cidade, estado, dataNascimento, email) VALUES ('Joao', '11111111111','90250180', 'Rua dos Bobos', 'São Leopoldo', 'RS', '2001-01-07T16:11:26.485','teste@gmail.com');
-INSERT INTO tb_usuario (nome, cpf, cep, logradouro, cidade, estado, dataNascimento, email) VALUES ('Stefano', '66363732026','90130000', 'Rua dos Malandros', 'Novo Hamburgo', 'RS', '2001-03-07T16:11:26.485','grupo5@gmail.com');
-INSERT INTO tb_usuario (nome, cpf, cep, logradouro, cidade, estado, dataNascimento, email) VALUES ('Guilherme', '35684995098','95000123', 'Rua dos Playboy', 'Porto Alegre', 'RS', '2001-04-07T16:11:26.485','grupo6@gmail.com');
->>>>>>> 740e93a6633f51459e3c0403d72ba3869ada4aa6
 
